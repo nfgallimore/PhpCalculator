25ea92e1d0c555db3e67f965bc1882ce965ef5d0
nfgallimore <nfgallimore@gmail.com>
2018-10-12 07:43:29 -0400

    Init

---
+++ pa3.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset = "utf-8" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />      
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+</head>
+<body>
+    <?php
+        $input = $_POST["calculator"];
+        $result = $_POST["result"];
+        
+        if ($result == null)
+        {
+            $result = 0.0;
+        }
+
+        if (isset($_POST['add']))
+        {
+            $result += $input;
+        }
+        else if (isset($_POST['sub']))
+        {
+            $result -= $input;
+        }
+        else if (isset($_POST['mul']))
+        {
+            $result *= $input;
+        }
+        else if (isset($_POST['div']))
+        {
+            $result /= $input;
+        }
+        else if (isset($_POST['eq']))
+        {
+            $result = $input;
+        }
+    ?>
+    <div class="container">
+        <h1>Calculator</h1>
+        <form method="post">
+            <legend>Calculator</legend>
+            <label for="calculator">Enter a number to calculate:</label>
+            <input type="text" name="calculator" id="calculator" class="form-control"/>
+            <input class="calc-button" type="submit" name="add" value="+">
+            <input class="calc-button" type="submit" name="sub" value="-">
+            <input class="calc-button" type="submit" name="mul" value="*">
+            <input class="calc-button" type="submit" name="div" value="/">
+            <input class="calc-button" type="submit" name="eq" value="="><br />
+            <label for="result">Result:</label>
+            <input type="text" id="result" name="result" class="form-control" readonly 
+                value="<?php print("$result"); ?>"" />
+        </form>
+    </div>
+</body> 
+</html>
